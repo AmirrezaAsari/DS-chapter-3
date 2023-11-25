1d903033e8f7deacde87a1d239b7df971bff34f6
unknown <amirreza.asari2@gmail.com>
2023-11-25 17:30:06 +0330

    q4

---
+++ q4.js
@@ -0,0 +1,13 @@
+const BigNumber = require('bignumber.js');
+
+
+function bigNumberFact(n){
+    x = new BigNumber(1);
+    for(let i=1; i<=n; i++){
+        x *= i;
+    }
+    return x;
+}
+
+
+console.log(bigNumberFact(20));
